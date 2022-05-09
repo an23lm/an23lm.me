3a473108d90d6e75e126ed4fa3bfba646640828f
Anselm Joseph <anselmjs@icloud.com>
2022-05-10 01:23:29 +0530

    feat: added links to email and github

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,36 +1,40 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import "./Contact.css";
 
 export default function Contact() {
-  return (
-    <div className="Contact-container">
-      <div className="Contact-content">
-        <div className="Contact-e-card">
-          <div className="Contact-card">
-            <div className="Contact-card-top">
-              <div className="Contact-card-lines">
-                <div className="Contact-card-line" />
-                <div className="Contact-card-line" />
-                <div className="Contact-card-line" />
-                <div className="Contact-card-line" />
-                <div className="Contact-card-line" />
-                <div className="Contact-card-line" />
-                <div className="Contact-card-line" />
-                <div className="Contact-card-line" />
-              </div>
-              {/* <div className="Contact-filler" /> */}
-              <div className="Contact-card-right">
-                <p className="Contact-card-link">an23lm@gmail.com</p>
-                <p className="Contact-card-link">github.com/an23lm</p>
-              </div>
-            </div>
-            <div className="Contact-card-bottom">
-              <p className="Contact-card-subtitle">Full Stack Developer</p>
-              <p className="Contact-card-title">Anselm Joseph</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="Contact-container">
+			<div className="Contact-content">
+				<div className="Contact-e-card">
+					<div className="Contact-card">
+						<div className="Contact-card-top">
+							<div className="Contact-card-lines">
+								<div className="Contact-card-line" />
+								<div className="Contact-card-line" />
+								<div className="Contact-card-line" />
+								<div className="Contact-card-line" />
+								<div className="Contact-card-line" />
+								<div className="Contact-card-line" />
+								<div className="Contact-card-line" />
+								<div className="Contact-card-line" />
+							</div>
+							{/* <div className="Contact-filler" /> */}
+							<div className="Contact-card-right">
+								<p className="Contact-card-link">
+									<a href="mailto:an23lm@gmail.com">an23lm@gmail.com</a>
+								</p>
+								<p className="Contact-card-link">
+									<a href="https://github.com/an23lm">github.com/an23lm</a>
+								</p>
+							</div>
+						</div>
+						<div className="Contact-card-bottom">
+							<p className="Contact-card-subtitle">Full Stack Developer</p>
+							<p className="Contact-card-title">Anselm Joseph</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
